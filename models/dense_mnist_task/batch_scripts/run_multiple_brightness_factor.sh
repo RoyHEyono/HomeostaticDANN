054a7356790003bf6a82d0ad4f6cd9689aa2ded1
RoyHEyono <roy.eyono@mila.quebec>
2024-06-04 09:19:08 -0400

    Created key figures

--- models/dense_mnist_task/batch_scripts/run_multiple_brightness_factor.sh
+++ models/dense_mnist_task/batch_scripts/run_multiple_brightness_factor.sh
@@ -13,8 +13,8 @@
 
 source /home/mila/r/roy.eyono/.conda/envs/ffcv_eg/bin/activate base
 
-brightness_eval_arr=(0 0.1 0.2 0.3 0.4 0.5 0.6 0.7 0.8 0.9 1)
+brightness_eval_arr=(0.0001 0.1 0.2 0.3 0.4 0.5 0.6 0.7 0.8 0.9 1)
 
 bright_eval=${brightness_eval_arr[$SLURM_ARRAY_TASK_ID]}
 
-python /home/mila/r/roy.eyono/HomeostaticDANN/models/dense_mnist_task/src/train.py --data.brightness_factor=0.75 --data.brightness_factor_eval=$bright_eval --train.dataset='fashionmnist' --opt.lr=0.01 --opt.wd=1e-6 --opt.inhib_momentum=0.5 --opt.momentum=0.9 --opt.inhib_lrs.wei=1e-2 --opt.inhib_lrs.wix=1 --train.batch_size=32 --exp.wandb_project=Luminosity_Brightness_Robustness --exp.wandb_entity=project_danns --exp.use_wandb=True
+python /home/mila/r/roy.eyono/HomeostaticDANN/models/dense_mnist_task/src/train.py --data.brightness_factor_eval=$bright_eval --model.normtype=ln_false --data.brightness_factor=0.75 --train.dataset=fashionmnist --opt.lr=0.01 --opt.wd=1e-6 --opt.inhib_momentum=0.9 --opt.momentum=0.5 --opt.inhib_lrs.wei=1e-2 --opt.inhib_lrs.wix=0.1 --train.batch_size=32 --exp.wandb_project=Luminosity_Brightness_Robustness --exp.wandb_entity=project_danns --exp.use_wandb=True
