9e1f79cc1095a07855de655ad6ebf8f5ef49b4b7
RoyHEyono <roy.eyono@mila.quebec>
2024-06-05 11:59:43 -0400

    Made changes to the brightness factor script

--- models/dense_mnist_task/batch_scripts/run_multiple_brightness_factor.sh
+++ models/dense_mnist_task/batch_scripts/run_multiple_brightness_factor.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-#SBATCH --array=0-10%11
+#SBATCH --array=0-20%21
 #SBATCH --partition=long
 #SBATCH --gres=gpu:rtx8000:1
 #SBATCH --mem=16GB
@@ -10,11 +10,12 @@
 #SBATCH --job-name=brightness_evaluation
 
 
+# There is a batch run in figures.ipynb to collect OOD performance of the models. Reason why there are so many arguments.
 
 source /home/mila/r/roy.eyono/.conda/envs/ffcv_eg/bin/activate base
 
-brightness_eval_arr=(0.0001 0.1 0.2 0.3 0.4 0.5 0.6 0.7 0.8 0.9 1)
+brightness_eval_arr=(-0.1 -0.2 -0.3 -0.4 -0.5 -0.6 -0.7 -0.8 -0.9 -1 0 0.1 0.2 0.3 0.4 0.5 0.6 0.7 0.8 0.9 1)
 
 bright_eval=${brightness_eval_arr[$SLURM_ARRAY_TASK_ID]}
 
-python /home/mila/r/roy.eyono/HomeostaticDANN/models/dense_mnist_task/src/train.py --data.brightness_factor_eval=$bright_eval --model.normtype=ln_false --data.brightness_factor=0.75 --train.dataset=fashionmnist --opt.lr=0.01 --opt.wd=1e-6 --opt.inhib_momentum=0.9 --opt.momentum=0.5 --opt.inhib_lrs.wei=1e-2 --opt.inhib_lrs.wix=0.1 --train.batch_size=32 --exp.wandb_project=Luminosity_Brightness_Robustness --exp.wandb_entity=project_danns --exp.use_wandb=True
+python /home/mila/r/roy.eyono/HomeostaticDANN/models/dense_mnist_task/src/train.py --data.brightness_factor_eval=$bright_eval --model.normtype=$2 --data.brightness_factor=$1 --train.dataset=fashionmnist --opt.lr=$3 --opt.wd=$4 --opt.inhib_momentum=$5 --opt.momentum=$6 --opt.inhib_lrs.wei=$7 --opt.inhib_lrs.wix=$8 --train.batch_size=32 --exp.wandb_project=Luminosity_Brightness_Robustness --exp.wandb_entity=project_danns --exp.use_wandb=True
